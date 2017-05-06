b2f648233e1255077822f04a9581654889de1891
Pontus Leitzler <leitzler@gmail.com>
2017-05-07 00:57:15 +0200

    Initial version

---
+++ main.go
@@ -0,0 +1,168 @@
+package main
+
+import (
+	"crypto/sha256"
+	"flag"
+	"fmt"
+	"os"
+	"os/exec"
+	"strconv"
+	"strings"
+	"syscall"
+	"time"
+
+	"github.com/atotto/clipboard"
+	"github.com/justwatchcom/gopass/password"
+	"github.com/marcusolsson/tui-go"
+)
+
+const logo = `
+  ________                                   
+ /  _____/  ____ ___________    ______ ______
+/   \  ___ /  _ \\____ \__  \  /  ___//  ___/
+\    \_\  (  <_> )  |_> > __ \_\___ \ \___ \ 
+ \______  /\____/|   __(____  /____  >____  >
+        \/       |__|       \/     \/     \/ `
+
+func main() {
+	unclipFlag := flag.Bool("u", false, "")
+	clipboardTimeout := flag.Int("t", 15, "Clipboard timeout in seconds")
+	flag.Parse()
+
+	// Process spawned to clear clipboard
+	if *unclipFlag {
+		unclip(*clipboardTimeout)
+		os.Exit(0)
+	}
+
+	store, err := password.NewRootStore(os.Getenv("HOME") + "/.password-store")
+	if err != nil {
+		panic(err)
+	}
+	secrets, err := store.List()
+	if err != nil {
+		panic(err)
+	}
+
+	status := tui.NewStatusBar("Enter - Copy to clipboard, Right Arrow - Show, ESC - Quit.")
+
+	storeList := tui.NewList()
+	storeList.AddItems(secrets...)
+	storeList.SetFocused(true)
+	storeList.SetSelected(0)
+
+	search := tui.NewEntry()
+	search.SetFocused(true)
+	search.OnChanged(func(entry *tui.Entry) {
+		foundItems := make([]string, 0)
+		allItems, err := store.List()
+		if err != nil {
+			status.SetText(err.Error())
+		}
+		for _, item := range allItems {
+			if strings.Contains(item, entry.Text()) {
+				foundItems = append(foundItems, item)
+			}
+		}
+		storeList.RemoveItems()
+		storeList.AddItems(foundItems...)
+		if len(foundItems) > 0 {
+			storeList.SetSelected(0)
+		}
+	})
+
+	form := tui.NewGrid(0, 0)
+	form.AppendRow(tui.NewLabel("Search:"))
+	form.AppendRow(search)
+
+	window := tui.NewVBox(
+		tui.NewPadder(10, 1, tui.NewLabel(logo)),
+		tui.NewPadder(1, 1, storeList),
+		tui.NewPadder(1, 1, form),
+	)
+	window.SetBorder(true)
+
+	wrapper := tui.NewVBox(
+		tui.NewSpacer(),
+		window,
+		tui.NewSpacer(),
+	)
+
+	content := tui.NewHBox(tui.NewSpacer(), wrapper, tui.NewSpacer())
+
+	root := tui.NewVBox(
+		content,
+		status,
+	)
+
+	ui := tui.New(root)
+	ui.SetKeybinding(tui.KeyEsc, func() { ui.Quit() })
+
+	// Show password
+	ui.SetKeybinding(tui.KeyArrowRight, func() {
+		secret, err := store.Get(storeList.SelectedItem())
+		if err != nil {
+			status.SetText(err.Error())
+			return
+		}
+		ui.Quit()
+		fmt.Printf("Password for %v: %v\n", storeList.SelectedItem(), string(secret))
+	})
+
+	// Copy to clipboard
+	storeList.OnItemActivated(func(list *tui.List) {
+		secret, err := store.Get(list.SelectedItem())
+		if err != nil {
+			status.SetText(err.Error())
+			return
+		}
+
+		if err := clipboard.WriteAll(string(secret)); err != nil {
+			panic(err)
+		}
+
+		err = delayedClearClipboard(secret, *clipboardTimeout)
+		if err != nil {
+			panic(err)
+		}
+
+		ui.Quit()
+		fmt.Printf("Password for %v copied to clipboard. Will clear in %v seconds.\n",
+			storeList.SelectedItem(),
+			*clipboardTimeout)
+	})
+
+	if err := ui.Run(); err != nil {
+		panic(err)
+	}
+}
+
+func delayedClearClipboard(content []byte, timeout int) error {
+	hash := fmt.Sprintf("%x", sha256.Sum256(content))
+
+	cmd := exec.Command(os.Args[0], "-u", strconv.Itoa(timeout))
+	// https://groups.google.com/d/msg/golang-nuts/shST-SDqIp4/za4oxEiVtI0J
+	cmd.SysProcAttr = &syscall.SysProcAttr{
+		Setpgid: true,
+	}
+	cmd.Env = append(os.Environ(), "GOPASS_TUI_UNCLIP_CHECKSUM="+hash)
+	return cmd.Start()
+}
+
+func unclip(timeout int) {
+	checksum := os.Getenv("GOPASS_TUI_UNCLIP_CHECKSUM")
+	time.Sleep(time.Second * time.Duration(timeout))
+
+	curr, err := clipboard.ReadAll()
+	if err != nil {
+		panic(err)
+	}
+
+	hash := fmt.Sprintf("%x", sha256.Sum256([]byte(curr)))
+
+	if hash == checksum {
+		if err := clipboard.WriteAll(""); err != nil {
+			panic(err)
+		}
+	}
+}
